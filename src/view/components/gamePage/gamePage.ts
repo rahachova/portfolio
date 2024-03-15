acd7d53208a5db581dac13ce2940ee5cfacf5c21
Aliaksandra Rahachova <111020592+rahachova@users.noreply.github.com>
2024-03-15 15:24:27 +0100

    feat: RSS-PZ-15 Add Drag-and-Drop (#56)
    * feat: add drag functionality from source to result block
    
    * feat: add full drag-and-drop functionality

--- src/view/components/gamePage/gamePage.ts
+++ src/view/components/gamePage/gamePage.ts
@@ -105,8 +105,11 @@ export default class GamePage extends Component {
         return arrayOfWords.map((word, index) => {
             const card = new Card(word, index);
             card.setAttribute('style', `width: ${cardWidths[index]}%;`);
+            card.setAttribute('draggable', 'true');
+            card.setAttribute('id', `${index}`);
             card.addListener('click', (event: Event) => this.moveCardToResultBlock(card, event));
             card.addListener('click', (event: Event) => this.moveCardToSourceBlock(card, event));
+            card.addListener('dragstart', (event) => GamePage.handleDragStart(event as DragEvent, card as Card));
             return card;
         });
     }
@@ -161,7 +164,7 @@ export default class GamePage extends Component {
         [...this.sourceBlock.getChildren()].forEach((card) => {
             this.moveCardToResultBlock(card as Card);
         });
-        this.activeResultBlock.setCorrectOrder()
+        this.activeResultBlock.setCorrectOrder();
     }
 
     initNextSentence() {
@@ -192,6 +195,69 @@ export default class GamePage extends Component {
         this.sourceBlock.appendChildren(GamePage.shuffleCards(this.createCards()));
     }
 
+    static handleDragStart(event: DragEvent, card: Card) {
+        // event.preventDefault();
+        event.dataTransfer?.setData('text', String(card.cardIndex));
+        event.dataTransfer!.dropEffect = 'move';
+    }
+
+    static handleDragover(event: DragEvent) {
+        event.preventDefault();
+        event.dataTransfer!.dropEffect = 'move';
+    }
+
+    handleResultDrop(event: DragEvent) {
+        event.preventDefault();
+        event.dataTransfer!.dropEffect = 'move';
+        const id = Number(event.dataTransfer?.getData('text'));
+        const cardToMove = (this.sourceBlock.getChildren() as Card[]).find((card: Card) => card.cardIndex === id) as Card;
+        if (!cardToMove) {
+            GamePage.rearangeInResultBlock(event, id, this.activeResultBlock);
+            return;
+        }
+        this.activeResultBlock.append(cardToMove);
+        this.sourceBlock.removeChild(cardToMove);
+        cardToMove.setIsUsed(true);
+        if (this.cardQuantity === this.activeResultBlock.getChildren().length) {
+            this.showCheckButton();
+        }
+    }
+
+    static rearangeInResultBlock(event: DragEvent, cardIndex: number, block: Component) {
+        if (!(event.target as HTMLElement).classList.contains('card')) {
+            return;
+        }
+        const targetWidth = (event.target as HTMLElement).offsetWidth;
+        const clickOffset = event.offsetX;
+        const isMoveBeforeTarget = clickOffset < targetWidth / 2;
+        const targedCardId = Number((event.target as HTMLElement).id);
+        const resultCards = block.getChildren() as Card[];
+        const cardToMoveIndex = resultCards.findIndex((card: Card) => card.cardIndex === cardIndex);
+        const [cardToMove] = resultCards.splice(cardToMoveIndex, 1);
+        const targedCardIndex = resultCards.findIndex((card: Card) => card.cardIndex === targedCardId);
+        const indexToMoveOn = isMoveBeforeTarget ? targedCardIndex : targedCardIndex + 1;
+        const newCards = [...resultCards.slice(0, indexToMoveOn), cardToMove, ...resultCards.slice(indexToMoveOn)];
+        block.destroyChildren();
+        block.appendChildren(newCards);
+    }
+
+    handleSourceDrop(event: DragEvent) {
+        event.preventDefault();
+        event.dataTransfer!.dropEffect = 'move';
+        const id = Number(event.dataTransfer?.getData('text'));
+        const cardToMove = (this.activeResultBlock.getChildren() as Card[]).find((card: Card) => card.cardIndex === id) as Card;
+        if (!cardToMove) {
+            GamePage.rearangeInResultBlock(event, id, this.sourceBlock);
+            return;
+        }
+        this.sourceBlock.append(cardToMove);
+        this.activeResultBlock.removeChild(cardToMove);
+        cardToMove.setIsUsed(false);
+        if (this.cardQuantity > this.activeResultBlock.getChildren().length) {
+            this.hideCheckButton();
+        }
+    }
+
     setupSubscribtion() {
         gameController.onGameStart(this.initGamePage.bind(this));
         loginController.onLogout(this.hideGamePage.bind(this));
@@ -201,6 +267,10 @@ export default class GamePage extends Component {
         this.checkButton.addListener('click', this.checkResult.bind(this));
         this.continueButton.addListener('click', this.handleContinueButton.bind(this));
         this.autoCompleteButton.addListener('click', this.handleAutoComplete.bind(this));
+        this.resultField.addListener('dragover', (event) => GamePage.handleDragover(event as DragEvent));
+        this.resultField.addListener('drop', (event) => this.handleResultDrop(event as DragEvent));
+        this.sourceBlock.addListener('dragover', (event) => GamePage.handleDragover(event as DragEvent));
+        this.sourceBlock.addListener('drop', (event) => this.handleSourceDrop(event as DragEvent));
     }
 
     build() {
--- src/view/components/startScreen/startScreen.ts
+++ src/view/components/startScreen/startScreen.ts
@@ -39,7 +39,8 @@ export default class StartScreen extends Component {
         this.setupSubscribtion();
         this.setupListeners();
         this.build();
-        // this.handleStart();
+        // TODO: remove
+        this.handleStart();
     }
 
     updateGreeting() {
