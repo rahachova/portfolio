24dfd50d3b96b9c5d00430010af68cbd5ab3ccce
Aliaksandra Rahachova <klaustrafobiyakl@gmail.com>
2024-03-23 17:08:07 +0100

    feat: add garage section

---
+++ src/app/components/carTrack/carTrack.ts
@@ -0,0 +1,61 @@
+import './carTrack.css';
+import Component from '../../common/component';
+import Button from '../../common/button/button';
+import { Car } from '../../types/types';
+
+export default class CarTrack extends Component {
+    carControls: Component;
+
+    selectButton: Button;
+
+    removeButton: Button;
+
+    carName: Component;
+
+    engineControls: Component;
+
+    startEngineButton: Component;
+
+    stopEngineButton: Component;
+
+    car: Component;
+
+    flag: Component;
+
+    carId: number;
+
+    constructor(car: Car) {
+        super({ tag: 'div', className: 'car-track', text: '' });
+        this.carControls = new Component({ tag: 'div', className: 'car_controls' });
+        this.selectButton = new Button({
+            text: 'SELECT',
+            style: 'blue',
+            onClick: () => {},
+        });
+        this.removeButton = new Button({
+            text: 'REMOVE',
+            style: 'blue',
+            onClick: () => {},
+        });
+        this.carName = new Component({ tag: 'p', text: car.name, className: 'car_name' });
+        this.engineControls = new Component({ tag: 'div', className: 'engine_controls' });
+        this.startEngineButton = new Component({ tag: 'button', text: 'A', className: 'engine_button' });
+        this.stopEngineButton = new Component({ tag: 'button', text: 'B', className: 'engine_button engine_button--inactive' });
+        this.car = new Component({ tag: 'div', className: 'car' });
+        this.flag = new Component({ tag: 'div', className: 'flag' });
+        this.carId = car.id;
+
+        this.setupAttributes(car.color);
+        this.build();
+    }
+
+    setupAttributes(color: string) {
+        this.car.setAttribute('style', `background-color: ${color};`);
+    }
+
+    build() {
+        this.carControls.appendChildren([this.selectButton, this.removeButton, this.carName]);
+        this.engineControls.appendChildren([this.startEngineButton, this.stopEngineButton]);
+        this.appendChildren([this.carControls, this.engineControls, this.car, this.flag]);
+    }
+}
--- src/app/components/garage/garage.ts
+++ src/app/components/garage/garage.ts
@@ -1,20 +1,63 @@
 import './garage.css';
 import Component from '../../common/component';
+import CarTrack from '../carTrack/carTrack';
+import Button from '../../common/button/button';
+import { Car } from '../../types/types';
 
 export default class Garage extends Component {
     garageHeader: Component;
 
     garagePageNumber: Component;
 
+    carTracks: Component;
+
+    pagination: Component;
+
+    prevButton: Button;
+
+    nextButton: Button;
+
+    currentPage: number = 1;
+
+    carsPerPage: number = 7;
+
+    totalPages: number = 0;
+
     constructor() {
         super({ tag: 'div', className: 'garage', text: '' });
         this.garageHeader = new Component({ tag: 'h2', text: 'Garage', className: 'garage_header' });
-        this.garagePageNumber = new Component({ tag: 'h2', text: 'Page #', className: 'garage_page-number' });
+        this.garagePageNumber = new Component({ tag: 'h2', text: `Page #${this.currentPage}`, className: 'garage_page-number' });
+        this.carTracks = new Component({ tag: 'div', className: 'car-tracks' });
+        this.pagination = new Component({ tag: 'div', className: 'pagination' });
+        this.prevButton = new Button({ style: 'green', text: 'PREV', onClick: () => {} });
+        this.nextButton = new Button({ style: 'green', text: 'NEXT', onClick: () => {} });
 
         this.build();
+        this.init();
+    }
+
+    async init() {
+        this.renderGarage();
+        this.renderCarTracks();
+    }
+
+    async renderGarage() {
+        const response = await fetch('http://127.0.0.1:3000/garage');
+        const cars = await response.json();
+        const totalCars = cars.length;
+        this.totalPages = Math.ceil(totalCars / this.carsPerPage);
+        this.garageHeader.setTextContent(`Garage (${cars.length})`);
+    }
+
+    async renderCarTracks() {
+        const response = await fetch(`http://127.0.0.1:3000/garage?_limit=${this.carsPerPage}&_page=${this.currentPage}`);
+        const cars = await response.json();
+        const carTracks = cars.map((car: Car) => new CarTrack(car));
+        this.carTracks.appendChildren(carTracks);
     }
 
     build() {
-        this.appendChildren([this.garageHeader, this.garagePageNumber]);
+        this.pagination.appendChildren([this.prevButton, this.nextButton]);
+        this.appendChildren([this.garageHeader, this.garagePageNumber, this.carTracks, this.pagination]);
     }
 }
